6422e51eed9a5cf96a75e4fd9d664dab204064ad
BlueFlamesDino <trevyndkarenke@gmail.com>
2026-01-02 23:56:42 -0600

    Setup router
    Setup router so now pages can be made

--- blueflamesdino-website/src/main.js
+++ blueflamesdino-website/src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//Pages
+import Index from "./pages/Index.vue"
+const routes = [
+    { paths: "/", component: Index },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+//When script loaded create this app using the router to decide what page to display
+createApp(App).use(router).mount('#app')
---
+++ blueflamesdino-website/src/pages/Index.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+    <p>This is the Index page (Home)</p>
+</template>
+<style lang="css">
+    
+</style>
